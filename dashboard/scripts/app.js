23b86c44538a3721740be72e67cd1ecf2f0fc995
mikedennis <mike.dennis@stratisplatform.com>
2019-03-30 14:22:56 -0600

    Add back all the PWA code

--- dashboard/scripts/app.js
+++ dashboard/scripts/app.js
@@ -227,10 +227,9 @@
       app.toggleAddDialog(true);
     }
 
-    // add this code in to make PWA
-    // if ('serviceWorker' in navigator) {
-    //   navigator.serviceWorker
-    //            .register('./service-worker.js')
-    //            .then(function() { console.log('Service Worker Registered'); });
-    // }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./service-worker.js')
+               .then(function() { console.log('Service Worker Registered'); });
+    }
 })();
