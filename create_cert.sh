bab5e105484fb0e269dc227c1c37d9c2c2b5ee9a
MikeDennis <Mike.Dennis@stratisplatform.com>
2019-04-13 15:44:44 -0600

    new cert

--- create_cert.sh
+++ create_cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "basicConstraints=CA:true" > options.txt
-echo "subjectAltName=DNS:localhost" >> options.txt
+echo "subjectAltName=DNS:localhost,IP:127.0.0.1" >> options.txt
 openssl genrsa -out key.pem 2048
 openssl req -subj '/CN=localhost' -new -days 3650 -key key.pem -out cert.csr
 openssl x509 -req -days 3650 -in cert.csr -signkey key.pem -extfile ./options.txt -out cert.pem
